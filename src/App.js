2f38c726eeab6da806f9ac4ec11f624d23f3da3e
Mike Sherokee <michael.sherokee@gmail.com>
2024-04-17 02:03:08 -0400

    Added Pages, Filtering of Games, Navbar

--- src/App.js
+++ src/App.js
@@ -25,9 +25,6 @@ function App() {
         <li class="nav-item">
           <a class="nav-link" href="/random">Random</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/pricing">Pricing</a>
-        </li>
       </ul>
     </div>
   </div>
